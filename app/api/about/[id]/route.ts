8aaf44c3843519812a141cde9ffd86521450e2b1
PrakashSingh65 <pratapsing5656@gmail.com>
2026-04-03 16:35:13 +0530

    feat: implement CRUD API routes for about and skill sections

---
+++ app/api/about/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { revalidatePath } from "next/cache";
+import { ConnectDB } from "@/lib/db";
+import About from "@/models/about.model";
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string }}) {
+    try {
+        await ConnectDB()
+
+        const { id } = params;
+
+        const existAbout = await About.findById(id)
+
+        if (!existAbout) {
+            return NextResponse.json({ error: "About section not found" }, { status: 404 });
+        }
+
+        const deletedAbout = await About.findByIdAndDelete(id);
+
+        if (!deletedAbout) {
+            return NextResponse.json({ error: "Failed to delete about section" }, { status: 500 });
+        }
+
+        revalidatePath("/admin-panel/about");
+        revalidatePath("/");
+        return NextResponse.json({ message: "About section deleted successfully" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to delete about section" }, { status: 500 });       
+    }
+}
---
+++ app/api/about/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { revalidatePath } from "next/cache";
+import { ConnectDB } from "@/lib/db";
+import About from "@/models/about.model";
+
+export async function POST(req: NextRequest) {
+    try {
+        await ConnectDB()
+
+        const existAbout = await About.findOne({});
+
+        if (existAbout) {
+            return NextResponse.json({ error: "About section already exists" }, { status: 400 });
+        }
+
+       const { desc } = await req.json();
+
+        if (!desc) {
+            return NextResponse.json({ error: "Description is required" }, { status: 400 });
+        }
+
+        const newAbout = await About.create({ desc });
+
+        revalidatePath("/admin-panel/about");
+        revalidatePath("/");
+        return NextResponse.json({data: newAbout}, { status: 201 });
+
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to create about section" }, { status: 500 });
+    }
+}
