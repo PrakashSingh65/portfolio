551a2c36bfdbfd8697ffcf433f11d0a53316d770
PrakashSingh65 <pratapsing5656@gmail.com>
2026-04-04 23:11:48 +0530

    feat: add Avatar component and create admin intro page skeleton

--- app/(admin)/admin-panel/intro/page.tsx
+++ app/(admin)/admin-panel/intro/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function AdminIntroPage() {
   return (
-    <div>AdminIntroPage</div>
+    <div className='flex items-center justify-center h-screen'>
+      <Button>Button</Button>
+    </div>
   )
 }
