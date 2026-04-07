3da9dc22df843ee7d7984c191f1581abb7e35182
PrakashSingh65 <pratapsing5656@gmail.com>
2026-04-07 21:45:41 +0530

    feat: implement sidebar navigation system with responsive UI components and mobile hook

--- app/(admin)/admin-panel/intro/page.tsx
+++ app/(admin)/admin-panel/intro/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function AdminIntroPage() {
   return (
-    <div className='flex items-center justify-center h-screen bg-red-500'>
+    <div className='flex items-center justify-center h-screen'>
       <Button>Button</Button>
     </div>
   )
