a9a4fc280694ac89499ba2aba5e663c426ee88ac
PrakashSingh65 <pratapsing5656@gmail.com>
2026-08-02 23:24:32 +0530

    feat: enhance NameAnimation component with additional motion effects for improved user experience

--- app/(main)/name_animation/page.tsx
+++ app/(main)/name_animation/page.tsx
@@ -5,7 +5,21 @@ import{motion} from 'framer-motion';
 export default function NameAnimation() {
     return (
         <div className="flex flex-col gap-2">
-            
+            <motion.h1
+                initial={{opacity:0, y: 20}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 0.8, ease: "easeOut"}}
+                className="text-4xl md:text-6xl front-bold text-white"
+                >
+                    Hi, I'm {""}
+                    <span className="text-[#00FF88] bg-clip-text">
+                        Prakash Singh
+                    </span>
+                </motion.h1>
+
+                <motion.p
+                initial={{}}
+
         </div>
     )
 }
