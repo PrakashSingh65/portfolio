95b8e81ee4bbc1314e078eb0bae985ac2e7f4a22
PrakashSingh65 <pratapsing5656@gmail.com>
2026-08-02 23:13:59 +0530

    feat: add initial NameAnimation component with framer-motion integration

---
+++ app/(main)/name_animation/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import{motion} from 'framer-motion';
+
+export default function NameAnimation() {
+    return (
+        <div className="flex flex-col gap-2">
+            
+        </div>
+    )
+}
