e226d961bafc8a2a14643276ef3e1a06aabe4773
PrakashSingh65 <pratapsing5656@gmail.com>
2026-08-31 12:44:43 +0530

    feat: replace ServicesPage with BlogPage in Home component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Intro from './intro/page'
 import AboutPage from './about/page'
 import ProjectsPage from './projects/page'
-import ServicesPage from './services/page'
 import ContactPage from './contact/page'
 import SkillPage from './skill/page'
+import BlogPage from './blog/page'
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
       <AboutPage/>
       <SkillPage/>
       <ProjectsPage/>
-      <ServicesPage/>
+      <BlogPage/>
       <ContactPage/>
       
     </div>
