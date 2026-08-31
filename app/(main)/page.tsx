1ab455da54b3cbed65e5cc1a9a125fb8618977c1
PrakashSingh65 <pratapsing5656@gmail.com>
2026-08-31 12:22:44 +0530

    feat: update SkillPage component with improved styling and remove color attributes from skills

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,12 +4,14 @@ import AboutPage from './about/page'
 import ProjectsPage from './projects/page'
 import ServicesPage from './services/page'
 import ContactPage from './contact/page'
+import SkillPage from './skill/page'
 
 export default function Home() {
   return (
     <div>
       <Intro/>
       <AboutPage/>
+      <SkillPage/>
       <ProjectsPage/>
       <ServicesPage/>
       <ContactPage/>
